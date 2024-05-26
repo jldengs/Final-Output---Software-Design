60a7dfb991827588467bc99bebd4702f254a34c4
jldengs <jldengs3310@gmail.com>
2024-05-26 16:30:07 +0800

    Add files via upload

---
+++ vans shop/app.js
@@ -0,0 +1,109 @@
+const wrapper = document.querySelector(".sliderWrapper");
+const menuItems = document.querySelectorAll(".menuItem");
+
+const products = [
+  {
+    id: 1,
+    title: "OLD SKOOL",
+    price: 70,
+    colors: [
+      {
+        code: "black",
+        img: "img/oldskool2.png",
+      },
+      {
+        code: "green",
+        img: "img/oldskool3.png",
+      },
+
+    ],
+  },
+  {
+    id: 2,
+    title: "SLIP-ON",
+    price: 55,
+    colors: [
+      {
+        code: "black",
+        img: "img/slip on2.png",
+      },
+      {
+        code: "grey",
+        img: "img/slip on3.png",
+      },
+    
+    ],
+  },
+  {
+    id: 3,
+    title: "AUTHENTIC",
+    price: 55,
+    colors: [
+      {
+        code: "black",
+        img: "img/authentic2.png",
+      },
+      {
+        code: "skyblue",
+        img: "img/authentic3.png",
+      },
+    ],
+  },
+];
+
+let choosenProduct = products[0];
+
+const currentProductImg = document.querySelector(".productImg");
+const currentProductTitle = document.querySelector(".productTitle");
+const currentProductPrice = document.querySelector(".productPrice");
+const currentProductColors = document.querySelectorAll(".color");
+const currentProductSizes = document.querySelectorAll(".size");
+
+menuItems.forEach((item, index) => {
+  item.addEventListener("click", () => {
+    //change the current slide
+    wrapper.style.transform = `translateX(${-100 * index}vw)`;
+
+    //change the choosen product
+    choosenProduct = products[index];
+
+    //change texts of currentProduct
+    currentProductTitle.textContent = choosenProduct.title;
+    currentProductPrice.textContent = "$" + choosenProduct.price;
+    currentProductImg.src = choosenProduct.colors[0].img;
+
+    //assing new colors
+    currentProductColors.forEach((color, index) => {
+      color.style.backgroundColor = choosenProduct.colors[index].code;
+    });
+  });
+});
+
+currentProductColors.forEach((color, index) => {
+  color.addEventListener("click", () => {
+    currentProductImg.src = choosenProduct.colors[index].img;
+  });
+});
+
+currentProductSizes.forEach((size, index) => {
+  size.addEventListener("click", () => {
+    currentProductSizes.forEach((size) => {
+      size.style.backgroundColor = "white";
+      size.style.color = "black";
+    });
+    size.style.backgroundColor = "black";
+    size.style.color = "white";
+  });
+});
+
+const productButton = document.querySelector(".productButton");
+const payment = document.querySelector(".payment");
+const close = document.querySelector(".close");
+
+productButton.addEventListener("click", () => {
+  payment.style.display = "flex";
+});
+
+close.addEventListener("click", () => {
+  payment.style.display = "none";
+});
